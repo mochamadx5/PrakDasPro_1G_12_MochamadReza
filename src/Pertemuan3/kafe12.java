a0fd3dadef9dae34d1a61337b53c8839bc0ef426
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-09-19 14:03:42 +0700

    kafejava12

--- src/Pertemuan3/kafe12.java
+++ src/Pertemuan3/kafe12.java
@@ -1,6 +1,7 @@
 package Pertemuan3;
 
 import java.util.Scanner;
+
 public class kafe12 {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
@@ -17,11 +18,13 @@ public class kafe12 {
         System.out.print("Masukkan jumlah pembelian roti: ");
         jmlRoti = input.nextInt();
         double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
-        double nominalBayar = totalHarga - (diskon * totalHarga);
+        byte totalByte = (byte) totalHarga;
+        double nominalBayar = totalHarga + (diskon * totalHarga);
         int nominalInt = (int) nominalBayar;
         System.out.println("Keanggotaan Pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh," + jmlRoti + " roti");
-        System.out.println("Nominal Bayar Rp " + nominalBayar);
+        System.out.println("Total Harga Rp " + totalByte);
+        System.out.println("Nominal Bayar Rp " + nominalInt);
 
     }
 }
