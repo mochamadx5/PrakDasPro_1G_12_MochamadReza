71979849ee66464005666df0911c3d188a884ab8
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-09-19 12:06:05 +0700

    p3-perobaan2-pertanyaanNO3

--- src/Pertemuan3/kafe12.java
+++ src/Pertemuan3/kafe12.java
@@ -18,6 +18,7 @@ public class kafe12 {
         jmlRoti = input.nextInt();
         double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
         double nominalBayar = totalHarga - (diskon * totalHarga);
+        int nominalInt = (int) nominalBayar;
         System.out.println("Keanggotaan Pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh," + jmlRoti + " roti");
         System.out.println("Nominal Bayar Rp " + nominalBayar);
