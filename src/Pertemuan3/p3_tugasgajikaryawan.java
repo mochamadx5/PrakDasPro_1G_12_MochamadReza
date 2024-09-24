58896f8c29b7cd9b2f9dcf3ee3dec2f97196d9f7
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-09-24 12:27:09 +0700

    “P5_Percobaan1”

--- src/Pertemuan3/p3_tugasgajikaryawan.java
+++ src/Pertemuan3/p3_tugasgajikaryawan.java
@@ -9,20 +9,30 @@ public class p3_tugasgajikaryawan {
 
     public static void main (String [] args) {
         Scanner input = new Scanner(System.in);
+        
+        // variabel 
         int jumlahJamKerja, upahPerJam;
         double gajiKaryawan, bonusGaji, gajiSetelahPajak, gajiSebelumPajak, gajiBersih;
+        
+        // input
         System.out.print("Masukkan jumlah jam kerja karyawan: ");
         jumlahJamKerja = input.nextInt();
         System.out.print("Masukkan upah per jam: ");
         upahPerJam = input.nextInt();
+
+        // menghitung gaji karyawan
         gajiKaryawan = jumlahJamKerja * upahPerJam;
         System.out.println("Gaji Karyawan: " + gajiKaryawan);
-        gajiSebelumPajak = gajiKaryawan * 0.1;
+        
+        bonusGaji = gajiKaryawan * 0.1;
         System.out.println("Bonus Gaji: " + bonusGaji);
+        
         gajiSebelumPajak = gajiKaryawan + bonusGaji;
         System.out.println("Gaji sebelum pajak: " + gajiSebelumPajak);
+        
         gajiSetelahPajak = bonusGaji * 0.05;
         System.out.println("Gaji Setelah Pajak: " + gajiSetelahPajak);
+        
         gajiBersih = (int) (gajiKaryawan - gajiSetelahPajak);
         System.out.println("Gaji bersih sebesar " + gajiBersih);
     }
--- src/pertemuan4/kuis1.java
+++ src/pertemuan4/kuis1.java
@@ -1,27 +1,22 @@
 package pertemuan4;
 
 import java.util.Scanner;
-/*
- * kuis1
- */
+
 public class kuis1 {
 
- public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    double tanggalBerangkat;
-    double bulanberangkat;
-    double tahunberangkat;
-    System.out.println("Nama : Mochamad Reza Firdaus");
-    System.out.println("NIM : 244107020104");
-    System.out.println("Kelas : 1G");
-    System.out.print("Masukkan tanggal berangkat: ");
-    tanggalBerangkat = sc.nextDouble();
-    System.out.print("Masukkkan bulan berangkat: ");
-    bulanberangkat = sc.nextDouble();
-    System.out.print("Masukkan tahun berangkat: ");
-    tahunberangkat = sc.nextDouble();
-    tanggalBerangkat = (tanggalBerangkat - bulanberangkat - tahunberangkat);
-    System.out.println("tanggal berangkat :" + tanggalBerangkat);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nama: Mochamad Reza Firdaus");
+        System.out.println("NIM: 244107020104");
+        System.out.println("Kelas: 1G");
+        System.out.print("Masukkan tanggal berangkat: ");
+        int tanggalBerangkat = sc.nextInt();
+        System.out.print("Masukkan bulan berangkat: ");
+        int bulanBerangkat = sc.nextInt();
+        System.out.print("Masukkan tahun berangkat: ");
+        int tahunBerangkat = sc.nextInt();
+        System.out.println("Tanggal keberangkatan: " + tanggalBerangkat + "-" + bulanBerangkat + "-" + tahunBerangkat);
 
+        sc.close();
     }
 }
