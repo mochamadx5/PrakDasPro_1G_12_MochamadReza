d79db753729b2089e1d2c353de9997df11ab2217
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-10-05 08:00:29 +0700

    Pertanyaan 1_no3

--- src/pertemuan6/Pemilihan2Percobaan112.java
+++ src/pertemuan6/Pemilihan2Percobaan112.java
@@ -11,6 +11,7 @@ public class Pemilihan2Percobaan112 {
         System.out.print("Masukkan tahun: ");
         int tahun = duaBelas.nextInt(); 
 
+
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println(tahun + " adalah tahun kabisat.");
