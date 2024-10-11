463d5a1f8797564cf0558a7ed0629ea1b301a89c
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-10-11 13:42:55 +0700

    jobsheet7_p1_pertanyaan5

--- src/pertemuan7/SiakadFor12.java
+++ src/pertemuan7/SiakadFor12.java
@@ -7,21 +7,33 @@ public class SiakadFor12 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0; 
+        int batasKelulusan = 60; 
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
             
+            
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
-        
+            
             if (nilai < terendah) {
                 terendah = nilai;
             }
-        }
+
+            if (nilai >= batasKelulusan) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            }
+        }        
         
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahTidakLulus);
     }
 }
