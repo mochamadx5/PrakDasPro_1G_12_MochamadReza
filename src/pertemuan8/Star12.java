c7cd4ae8e85c8628ee8444c02896765ff739f391
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-10-28 18:48:08 +0700

    Jobsheet8_p2_pertanyaanNo5

--- src/pertemuan8/Star12.java
+++ src/pertemuan8/Star12.java
@@ -6,7 +6,7 @@ public class Star12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
     
-        System.out.print("Masukkan nilai N = ");
+        System.out.print("Masukkan Nilai N = ");
         int N = sc.nextInt();
 
         for(int iOuter=1; iOuter<=N; iOuter++){
