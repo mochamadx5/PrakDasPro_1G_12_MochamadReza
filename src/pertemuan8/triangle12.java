ebf70e39d4ad7ba87ac3057296c940c8c88c0fa1
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-10-29 10:29:10 +0700

    Jobsheet8_percobaan3_pertanyaanNO3

---
+++ src/pertemuan8/triangle12.java
@@ -0,0 +1,23 @@
+package pertemuan8;
+
+import java.util.Scanner;
+
+public class triangle12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan nilai N = ");
+        int N = sc.nextInt();
+
+        int i = 1;
+        while (i<=N) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    
+}
