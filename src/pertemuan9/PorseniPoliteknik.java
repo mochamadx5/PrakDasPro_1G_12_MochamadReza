236d7ba27eed1d40227104cb035d32f1fbd56e66
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-11-01 13:24:55 +0700

    Jobsheet8_TugasNO2

---
+++ src/pertemuan9/PorseniPoliteknik.java
@@ -0,0 +1,47 @@
+package pertemuan9;
+
+import java.util.Scanner;
+
+public class PorseniPoliteknik {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah politeknik yang berpartisipasi: ");
+        int jumlahPoliteknik = Integer.parseInt(scanner.nextLine());
+
+        int politeknikCounter = 0;
+        while (politeknikCounter < jumlahPoliteknik) {
+            System.out.print("Masukkan nama Politeknik: ");
+            String namaPoliteknik = scanner.nextLine();
+
+            int caborCounter = 0;
+            while (caborCounter < 4) {
+                String cabor = "";
+                if (caborCounter == 0) {
+                    cabor = "Badminton";
+                } else if (caborCounter == 1) {
+                    cabor = "Tenis Meja";
+                } else if (caborCounter == 2) {
+                    cabor = "Basket";
+                } else if (caborCounter == 3) {
+                    cabor = "Bola Voli";
+                }
+
+                System.out.println("Masukkan 5 nama atlet untuk cabang " + cabor + " dari " + namaPoliteknik + ":");
+                int atletCounter = 0;
+                while (atletCounter < 5) {
+                    System.out.print("Nama atlet " + (atletCounter + 1) + ": ");
+                    String namaAtlet = scanner.nextLine();
+                    System.out.println("Data disimpan: " + namaPoliteknik + " - " + cabor + " - " + namaAtlet);
+                    atletCounter++;
+                }
+
+                caborCounter++;
+            }
+
+            politeknikCounter++;
+        }
+
+        scanner.close();
+    }
+}
