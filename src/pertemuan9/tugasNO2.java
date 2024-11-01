a178865fa50135ef1a77e436aa08e97a064a5922
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-11-01 13:32:50 +0700

    Jobsheet8_percobaan9_tugasNO2

--- src/pertemuan9/tugasNO2.java
+++ src/pertemuan9/tugasNO2.java
@@ -2,7 +2,7 @@ package pertemuan9;
 
 import java.util.Scanner;
 
-public class PorseniPoliteknik {
+public class tugasNO2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
