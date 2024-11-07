1f1cf67fad3f5f33bc942756d279e38db8f449b5
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-11-07 11:01:32 +0700

    P10_percobaan4_PertanyaanNO3

--- src/pertemuan10/SearchNilai12.java
+++ src/pertemuan10/SearchNilai12.java
@@ -1,20 +1,39 @@
 package pertemuan10;
 
+import java.util.Scanner;
+
 public class SearchNilai12 {
     public static void main(String[] args) {
-        int[] ArrayNilai12 = {80, 85, 78,96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("masukkan banyaknya nilai yang akan diinput: ");
+        int jumlahNilai = sc.nextInt();
+        int[] ArrayNilai12 = new int[jumlahNilai];
 
-        for (int i = 0; i < ArrayNilai12.length; i++) {
-            if (key == ArrayNilai12[i]) {
-                hasil = i;
+        for (int i = 0; i < jumlahNilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ":"); 
+            ArrayNilai12[i] = sc.nextInt();
+        }
+
+        System.out.print("masukkan nilai yang ingin dicari : ");
+        int key = sc.nextInt();
+        int hasil = -1;
+
+        for (int j = 0; j < ArrayNilai12.length; j++) {
+            if (key == ArrayNilai12[j]) {
+                hasil = j;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-" + hasil);
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1 ));
+        } else {
+            System.out.println(" nilai yang dicari tidak ditemukan.");
+        
         System.out.println();
+
+        }
     }
     
 }
