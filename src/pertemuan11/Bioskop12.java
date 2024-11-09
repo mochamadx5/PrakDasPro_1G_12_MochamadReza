6b2ae36ab2f869c1be00edd359d34eea7461011f
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-11-09 20:12:26 +0700

    P11_Percobaan1

--- src/pertemuan11/Bioskop12.java
+++ src/pertemuan11/Bioskop12.java
@@ -21,8 +21,10 @@ public class Bioskop12 {
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Penonton pada baris ke- " + (i+1) + ": " + String.join(", ", penonton[i]));
         }
-        }
-        }
+
+    }
+    
+}
     
    
 
