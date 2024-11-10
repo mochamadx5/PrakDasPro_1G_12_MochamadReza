536e956ce6fd79f7922525c3f058793078c5c7d2
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-11-10 19:27:10 +0700

    P11_PERCOBAAN4

---
+++ src/pertemuan11/SIAKAD12.java
@@ -0,0 +1,42 @@
+package pertemuan11;
+
+import java.util.Scanner;
+
+public class SIAKAD12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah siswa : ");
+        int jumlahSiswa = sc.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah : ");
+        int jumlahMatkul = sc.nextInt();
+
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("Input nilai mahasiswa ke- " + (i + 1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j < jumlahMatkul; j++) {
+                System.out.print("Nilai Mata Kuliah " + (j + 1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+
+            System.out.println("Nilai rata-rata mahasiswa ke- " + (i + 1) + ": " +  totalPerSiswa / jumlahMatkul);
+        }
+        
+        System.out.println("\n===============================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
+        
+        for (int j = 0; j < jumlahMatkul; j++) {
+            double totalPerMatkul = 0;
+
+            for (int i = 0; i < jumlahSiswa; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jumlahSiswa);
+        }
+    }
+}
