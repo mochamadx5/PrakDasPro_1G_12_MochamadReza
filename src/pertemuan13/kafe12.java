1c26c9d608d3145a8e3547f9ec75d218b66a1b54
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2024-11-29 13:40:05 +0700

    P13_Percobaan1

---
+++ src/pertemuan13/kafe12.java
@@ -0,0 +1,18 @@
+package pertemuan13;
+
+public class kafe12 {
+    public static void Menu () {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappucino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh tarik - Rp 12,000");
+        System.out.println("5. Roti bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("==========================");
+        System.out.println("Silakan pilin menu yang anda inginkan");
+    }   
+    public static void main (String[] args){
+        Menu();
+    }
+}
