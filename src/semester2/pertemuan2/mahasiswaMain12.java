1a970554c07fa098ff3124777a4b1b1ecd3703c5
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-02-20 10:04:25 +0700

    pertemuan2_praktikum3

--- src/semester2/pertemuan2/mahasiswaMain12.java
+++ src/semester2/pertemuan2/mahasiswaMain12.java
@@ -16,5 +16,9 @@ public class mahasiswaMain12 {
         Mahasiswa12 mhs2 = new Mahasiswa12("Annisa Nabila", "2141720160", 3.25, "TI 2L");
         mhs2.updateIPK(3.30);
         mhs2.tampilkanInformasi();
+
+        Mahasiswa12 mhsReza = new Mahasiswa12("Mochamad Reza Firdaus", "21311421140", 3.36, "TI 1H");
+        mhsReza.updateIPK(3.30);
+        mhsReza.tampilkanInformasi();
     }
 }
