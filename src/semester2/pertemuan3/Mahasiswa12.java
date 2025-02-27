d04702af62845cb1feeef6a3927f54bb5d559f32
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-02-27 11:24:23 +0700

    P3_tugas1

--- src/semester2/pertemuan3/Mahasiswa12.java
+++ src/semester2/pertemuan3/Mahasiswa12.java
@@ -1,8 +1,12 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 package semester2.pertemuan3;
 
 public class Mahasiswa12 {
-    public String nim;
-    public String nama;
-    public String kelas;
-    public float ipk;
+   public String nim;
+   public String nama;
+   public String kelas;
+   public float ipk;
+
+   public Mahasiswa12() {
+   }
 }
--- src/semester2/pertemuan3/MataKuliah12.java
+++ src/semester2/pertemuan3/MataKuliah12.java
@@ -1,15 +1,41 @@
 package semester2.pertemuan3;
 
+import java.util.Scanner;
+
 public class MataKuliah12 {
+    private static Scanner sc = new Scanner(System.in); // Scanner hanya dibuat sekali
+
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
 
+    // Constructor default
+    public MataKuliah12() {}
+
+    // Constructor dengan parameter
     public MataKuliah12(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+    public void tambahData() {
+        System.out.print("Kode       : ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        this.nama = sc.nextLine();
+        System.out.print("SKS        : ");
+        this.sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah Jam : ");
+        this.jumlahJam = Integer.parseInt(sc.nextLine());
+        System.out.println("------------------------");
+    }
+    public void cetakInfo() {
+        System.out.println("Kode       : " + this.kode);
+        System.out.println("Nama       : " + this.nama);
+        System.out.println("SKS        : " + this.sks);
+        System.out.println("Jumlah Jam : " + this.jumlahJam);
+        System.out.println("------------------------");
     }
 }
