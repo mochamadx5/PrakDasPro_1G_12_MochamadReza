790b876c859ca5bab448a35d946e0dd4871ceb23
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-03-13 09:10:21 +0700

    P5_Perobaan1

--- src/semester2/BruteForceDivideConquer/mainFaktorial12.java
+++ src/semester2/BruteForceDivideConquer/mainFaktorial12.java
@@ -1,15 +1,18 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 package semester2.BruteForceDivideConquer;
 
 import java.util.Scanner;
 
 public class mainFaktorial12 {
-    public static void main(String[] args) {
-        Scanner input12 = new Scanner(System.in);
-        System.out.print("Masukkan nilai : ");
-        int nilai = input12.nextInt();
+   public mainFaktorial12() {
+   }
 
-        faktorial12 fk = new faktorial12();
-        System.out.println("Nilai faktorial " + nilai + " menggunakan BF :" + fk.faktorialBF(nilai));
-        System.out.println("Nilai faktorial "+ nilai + " menggunakan DC :" + fk.faktorialDC(nilai));
-    }
+   public static void main(String[] args) {
+      Scanner input12 = new Scanner(System.in);
+      System.out.print("Masukkan nilai : ");
+      int nilai = input12.nextInt();
+      faktorial12 fk = new faktorial12();
+      System.out.println("Nilai faktorial " + nilai + " menggunakan BF :" + fk.faktorialBF(nilai));
+      System.out.println("Nilai faktorial " + nilai + " menggunakan DC :" + fk.faktorialDC(nilai));
+   }
 }
