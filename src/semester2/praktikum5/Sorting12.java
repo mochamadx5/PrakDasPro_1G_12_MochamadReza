fe03706328b34952da885a9b041653f14d62d50e
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-03-20 13:53:38 +0700

    P5_percobaan1

--- src/semester2/praktikum5/Sorting12.java
+++ src/semester2/praktikum5/Sorting12.java
@@ -66,7 +66,7 @@ public class Sorting12 {
         int b[]= {30, 20, 2, 8, 14};
         Sorting12 dataurut2 = new Sorting12(b, b.length);
 
-        System.out.println("Data awal 2");
+        System.out.println("data awal 2");
         dataurut2.tampil();
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
